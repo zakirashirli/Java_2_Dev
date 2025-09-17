bf0050aba7680365bda6c7f6d20a6a83880c988d
zakirashirli <117630695+zakirashirli@users.noreply.github.com>
2025-09-17 21:05:15 +0400

    Operators
    +-/*%

--- Java_Rus_Dea/src/Main.java
+++ Java_Rus_Dea/src/Main.java
@@ -2,14 +2,38 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int a = 5 / 2;
+        System.out.println(a);
+
+        int b = 5 + 1;
+
+        b += 1; // +1
+        b -= 1; // -1
+        b *= 1; // *1
+        b /= 1; // /1 = b
+        b %= 1; // %1
+
+
+        System.out.println(b);
+        int num = 100;
+        num += 5;
+
+        System.out.println(num);
+
+
+        int x = 10;
+
+        ++x;
+        System.out.println(x);
+        System.out.println(x);
+        boolean first = true; // !first true->false
+        boolean second = false; // !second false->true
+        if(5 != 4) {
+            System.out.println("true");
+        }else{
+            System.out.println("false");
         }
+
     }
 }
